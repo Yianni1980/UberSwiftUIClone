712500ab4ea859f368843735a862b111d400e0d2
yianni1980 <ioagian1980@gmail.com>
2023-05-02 23:51:30 +0300

    Initiated mapView polyline to draw a route

--- uberSwiftUITutorial/Core/LocationSearch/View Models/LocationSearchViewModel.swift
+++ uberSwiftUITutorial/Core/LocationSearch/View Models/LocationSearchViewModel.swift
@@ -13,7 +13,7 @@ class LocationSearchViewModel:NSObject,ObservableObject {
     //MARK: - Properties
     
     @Published var results = [MKLocalSearchCompletion]()
-    @Published var selectedLocation:String?
+    @Published var selectedLocationCoordinate:CLLocationCoordinate2D?
     private let searchCompleter = MKLocalSearchCompleter()
     var queryFragment:String = "" {
         didSet {
@@ -31,11 +31,31 @@ class LocationSearchViewModel:NSObject,ObservableObject {
     
     //MARK: Helpers
     
-    func selectLocation(_ Location:String) {
-        self.selectedLocation = Location
+    func selectLocation(_ localSearch:MKLocalSearchCompletion) {
+        locationSearch(forlocalSearchCompletion: localSearch) { response, error in
+            if let error = error {
+                print("DEBUG: :Location search failed with error \(error.localizedDescription)")
+                return
+            }
+            guard let item = response?.mapItems.first else {return}
+            let coordinate = item.placemark.coordinate
+            self.selectedLocationCoordinate = coordinate
+        }
         
-        print("DEBUG:Selected location is \(self.selectedLocation) ")
+        
     }
+    
+    
+    func locationSearch(forlocalSearchCompletion localSearch:MKLocalSearchCompletion,
+                        completion:@escaping MKLocalSearch.CompletionHandler) {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = localSearch.title.appending(localSearch.subtitle)
+        let search = MKLocalSearch(request: searchRequest)
+        search.start(completionHandler: completion)
+        
+    }
+    
+    
     
 }
 extension LocationSearchViewModel:MKLocalSearchCompleterDelegate {
